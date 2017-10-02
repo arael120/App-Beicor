bf2369ede8b98a452e310b839a2e0dcc954dc1ac
Mephisto120 <hunterprice@gmail.com>
2017-10-02 02:31:21 -0500

    add -f

---
+++ www/assets/js/settings.js
@@ -0,0 +1,12 @@
+var wordpress_url = "http://www.beicor.mx";
+var wordpress_per_page = 10;
+var request_timeout = 30000;
+var open_target_blank = true;
+var date_format = "d MMM y HH:mm";
+var onesignal_app_id = "4b6e36a3-a423-4c29-926d-55b8b4f627ae";
+var application_language = "en";
+var google_analytics = "GOOGLE_ANALYTICS"; 
+var admob_android_banner = '';
+var admob_android_interstitial = '';
+var admob_ios_banner = '';
+var admob_ios_interstitial = '';
